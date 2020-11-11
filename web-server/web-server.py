e22a1d6f69a1b2781eea7491c41a184dd6dfeccd
Allen Myers <3505537+armyers@users.noreply.github.com>
2020-11-10 18:10:32 -0800

    web-server is now threaded

--- web-server/web-server.py
+++ web-server/web-server.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python3
-"""
-Very simple HTTP server in python for logging requests
-Usage::
-    ./server.py [<port>]
-"""
-from http.server import BaseHTTPRequestHandler, HTTPServer, ThreadingHTTPServer
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from socketserver import ThreadingMixIn
+import threading
 import socket
 import logging
 
@@ -24,46 +20,36 @@ def get_server_ip():
         s.close()
     return IP
 
-class S(BaseHTTPRequestHandler):
-    def _set_response(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self._set_response()
+        self.send_response(200)
+        self.end_headers()
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         # XXXX using a global function and global var -- probably not cool!
         self.wfile.write(bytes("<p>web server: %s:%s</p>" % (get_server_ip(), SERVER_PORT), "utf-8"))
+        self.wfile.write(bytes("<p>Server Threads: %s, %s total threads</p>" % (threading.currentThread().getName(), str(threading.active_count())), "utf-8"))
         self.wfile.write(bytes("<p>Host header: %s</p>" % (self.headers['Host']), "utf-8"))
         self.wfile.write(bytes("<p>client address: %s:%s</p>" % (self.client_address[0], self.client_address[1]), "utf-8"))
         self.wfile.write(bytes("<p>X-Forwarded-For header: %s</p>" % (self.headers['X-Forwarded-For']), "utf-8"))
         self.wfile.write(bytes("<p>X-Forwarded-Proto header: %s</p>" % (self.headers['X-Forwarded-Proto']), "utf-8"))
         self.wfile.write(bytes("<p>The time is now: %s</p>" % self.date_time_string(), "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
-        logging.info("GET request:\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
 
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
+class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
+    pass
 
-        self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
-
-def run(server_class=ThreadingHTTPServer, handler_class=S, port=SERVER_PORT):
-    logging.basicConfig(level=logging.INFO)
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    logging.info('Starting httpd...\n')
+def run():
+    server = ThreadingSimpleServer(('', SERVER_PORT), Handler)
+    logging.info('Starting web-server...\n')
     try:
-        httpd.serve_forever()
+        server.serve_forever()
     except KeyboardInterrupt:
         pass
-    httpd.server_close()
-    logging.info('Stopping httpd...\n')
+    server.server_close()
+    logging.info('Stopping web-server...\n')
 
 if __name__ == '__main__':
     run()
+
